de0cfae7e42f3f6b2beec8ebb27436412f80d1d7
PizzAna <anttikiviyo@gmail.com>
2014-12-29 15:57:19 +0200

    Remove the weakPrimalObject

--- src/main/java/atomatic/init/ModItems.java
+++ src/main/java/atomatic/init/ModItems.java
@@ -3,7 +3,6 @@ package atomatic.init;
 import atomatic.item.ItemA;
 import atomatic.item.ItemCreativeTab;
 import atomatic.item.ItemPrimalObject;
-import atomatic.item.ItemWeakPrimalObject;
 import atomatic.reference.Names;
 import atomatic.reference.Reference;
 
@@ -14,12 +13,10 @@ public class ModItems
 {
     public static final ItemCreativeTab creativeTab = new ItemCreativeTab();
     public static final ItemA primalObject = new ItemPrimalObject();
-    public static final ItemA weakPrimalObject = new ItemWeakPrimalObject();
 
     public static void init()
     {
         GameRegistry.registerItem(creativeTab, Names.Items.CREATIVE_TAB);
         GameRegistry.registerItem(primalObject, Names.Items.PRIMAL_OBJECT);
-        GameRegistry.registerItem(weakPrimalObject, Names.Items.WEAK_PRIMAL_OBJECT);
     }
 }
